ef208221778a7d39b320d9e9d839f90bda704caf
David Kindler <david.kindler@nxp.com>
2018-02-18 13:48:09 -0600

    Animate backdrop

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => {
-    const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
-
-    return <div className={cssClasses.join(' ')}></div>;
+const backdrop = props => {
+  const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
+  const backgroundShaded = props.light ? { backgroundColor: 'rgba(0,0,0,0)' } : { backgroundColor: 'rgba(0,0,0,0.8)' };
+  return <div className={cssClasses.join(' ')} style={backgroundShaded} />;
 };
 
 export default backdrop;
